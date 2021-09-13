b631394c9d7363b588b6d62b711e75d41b8c425b
kurban1998 <kurban.kurban1998@mail.ru>
2021-09-13 16:10:52 +0300

    BankiDz
    BankiDz

---
+++ bank/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BANKI_DZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 bank = new Class1();
+            bank.DataAboutPerson();
+        }
+    }
+}
